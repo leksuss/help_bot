8d8e419fbc74a6a0350520d7c8fdedc684c54884
Andrey Alekseev <leksus@leksus.info>
2023-08-24 15:05:01 +0300

    add try/except to prevent down due to vk.com timeout

--- vk_bot.py
+++ vk_bot.py
@@ -13,17 +13,20 @@ logger = logging.getLogger(__name__)
 
 
 def send_reply(event, bot, project_id):
-    query_result = get_dialogflow_answer(
-        event.user_id,
-        event.text,
-        project_id,
-    )
-    if not query_result.intent.is_fallback:
-        bot.messages.send(
-            user_id=event.user_id,
-            message=query_result.fulfillment_text,
-            random_id=random.randint(1,1000),
+    try:
+        query_result = get_dialogflow_answer(
+            event.user_id,
+            event.text,
+            project_id,
         )
+        if not query_result.intent.is_fallback:
+            bot.messages.send(
+                user_id=event.user_id,
+                message=query_result.fulfillment_text,
+                random_id=random.randint(1,1000),
+            )
+    except Exception as e:
+        logger.exception(e)
 
 
 def main():
