b2558b5afc9903d4fefeb4f5a922ff86fe72d5e6
Andrey Alekseev <leksus@leksus.info>
2023-08-26 12:19:18 +0300

    move try/except to whole vk communictation

--- vk_bot.py
+++ vk_bot.py
@@ -13,20 +13,17 @@ logger = logging.getLogger(__name__)
 
 
 def send_reply(event, bot, project_id):
-    try:
-        query_result = get_dialogflow_answer(
-            event.user_id,
-            event.text,
-            project_id,
+    query_result = get_dialogflow_answer(
+        event.user_id,
+        event.text,
+        project_id,
+    )
+    if not query_result.intent.is_fallback:
+        bot.messages.send(
+            user_id=event.user_id,
+            message=query_result.fulfillment_text,
+            random_id=random.randint(1,1000),
         )
-        if not query_result.intent.is_fallback:
-            bot.messages.send(
-                user_id=event.user_id,
-                message=query_result.fulfillment_text,
-                random_id=random.randint(1,1000),
-            )
-    except Exception as e:
-        logger.exception(e)
 
 
 def main():
@@ -48,12 +45,12 @@ def main():
 
     logger.info('Bot vk_bot started')
 
-    for event in longpoll.listen():
-        if event.type == VkEventType.MESSAGE_NEW and event.to_me:
-            try:
+    try:
+        for event in longpoll.listen():
+            if event.type == VkEventType.MESSAGE_NEW and event.to_me:
                 send_reply(event, bot, env('GOOGLE_CLOUD_PROJECT'))
-            except Exception as e:
-                logger.exception(e)
+    except Exception as e:
+        logger.exception(e)
 
 
 if __name__ == "__main__":
